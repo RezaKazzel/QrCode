e52c3dc9d1d0a7543d8344da3d1f3e08dcc50d57
ja <58966761+RezaKazzel@users.noreply.github.com>
2022-09-02 22:12:43 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+generateQR = () => {
+const text = document.getElementById("text").value;
+document.getElementById("img").src = "https://api.qrserver.com/v1/create-qr-code/?size=240×240&data=" + text; 
+}
