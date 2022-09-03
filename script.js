26eb774d2d5f7fed52bf4741ec0cae71a6bb6aaa
ja <58966761+RezaKazzel@users.noreply.github.com>
2022-09-03 07:34:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function myFunction() {
+  //document.getElementById("demo").innerHTML = "Hello World";
+  alert("Hello World");
+}
+
 generateQR = () => {
 const text = document.getElementById("text").value;
 document.getElementById("img").src = "https://api.qrserver.com/v1/create-qr-code/?size=240×240&data=" + text; 
